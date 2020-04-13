5a35cb938bc7387e98bc265b4434fe8430e7aa9d
sc17bh <sc17bh@leeds.ac.uk>
2020-04-13 09:17:29 +0100

    Added logic and button to generate a QR Code

--- app/src/main/java/com/uni/flightfinder/FlightInfo.kt
+++ app/src/main/java/com/uni/flightfinder/FlightInfo.kt
@@ -2,6 +2,11 @@ package com.uni.flightfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class FlightInfo : AppCompatActivity() {
 
@@ -9,5 +14,12 @@ class FlightInfo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flight_info)
     }
-}
 
+    // To be called by the onClick of a button in the layout
+    fun generateQRCode(view: View) {
+        val flightString = "this is a longer test string"
+        val encoder = BarcodeEncoder()
+        val qrCode = encoder.encodeBitmap(flightString, BarcodeFormat.QR_CODE, 500, 500)
+
+    }
+}
